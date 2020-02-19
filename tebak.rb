9b5d02e9f32e667a9aed9f452c48a39c4f0d8c50
Satriyo <satrio.nr27@gmail.com>
2020-02-19 15:18:53 +0700

    RubyTebakKata

---
+++ tebak.rb
@@ -0,0 +1,66 @@
+kata = ['BUUK','UUBK'].sample
+poin = 0
+puts "1. tebak kata =#{kata}"
+jawab = gets.chomp
+if jawab == "BUKU"
+    poin = poin+1
+    puts "Poin Mu #{poin}"
+else
+    puts "salah"
+    puts "Poin Mu #{poin}"
+end
+
+kata = ['SAPU','UPSA'].sample
+puts "2. tebak kata =#{kata}"
+jawab = gets.chomp
+if jawab == "SAPU"
+    poin = poin+1
+    puts "Poin Mu #{poin}"
+else
+    puts "salah"
+    puts "Poin Mu #{poin}"
+end
+
+kata = ['ASPI','SAIP'].sample
+puts "3. tebak kata =#{kata}"
+jawab = gets.chomp
+if jawab == "SAPI"
+    poin = poin+1
+    puts "Poin Mu #{poin}"
+else
+    puts "salah"
+    puts "Poin Mu #{poin}"
+end
+
+kata = ['HRIA','RIAH'].sample
+puts "3. tebak kata =#{kata}"
+jawab = gets.chomp
+if jawab == "HARI"
+    poin = poin+1
+    puts "Poin Mu #{poin}"
+else
+    puts "salah"
+    puts "Poin Mu #{poin}"
+end
+
+kata = ['SAPU','UPSA'].sample
+puts "4. tebak kata =#{kata}"
+jawab = gets.chomp
+if jawab == "SAPU"
+    poin = poin+1
+    puts "Poin Mu #{poin}"
+else
+    puts "salah"
+    puts "Poin Mu #{poin}"
+end
+
+kata = ['BAUJ','UJAB'].sample
+puts "5. tebak kata =#{kata}"
+jawab = gets.chomp
+if jawab == "BAJU"
+    poin = poin+1
+    puts "Poin Mu #{poin}"
+else
+    puts "salah"
+    puts "Poin Mu #{poin}"
+end
